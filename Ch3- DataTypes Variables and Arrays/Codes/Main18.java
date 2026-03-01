2df490aae65ba37c537c59cef9bba6fb47dc1d39
Aditya Kumar <adii.utsav@gmail.com>
2026-03-01 16:49:34 +0530

    Questions on 2D arays

---
+++ Ch3- DataTypes Variables and Arrays/Codes/Main18.java
@@ -0,0 +1,24 @@
+// Scalar multiplication
+public class Main18 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        //Logic
+        int scalar = 2;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] *= scalar;
+            }
+        }
+        // Printing
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

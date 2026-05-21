6ec2990bb6246f877a2dfc5e8e9829a8ea3a0a46
Aditya Kumar <adii.utsav@gmail.com>
2026-05-21 08:48:29 +0530

    Added the topics when the for loop dont even runs

---
+++ Ch5-Control Statements/Codes/Example51_forloop.java
@@ -0,0 +1,9 @@
+//  The loop never runs as condition is false
+
+public class Example51_forloop {
+    public static void main(String[] args) {
+        for(int i = 10; i<1; i++){
+            System.out.println(i);
+        }
+    }
+}
